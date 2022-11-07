306f8774aaea2bd8f3f3afcac53e21da484ace49
Anish Gupta <anishgupta675@gmail.com>
2022-11-07 20:15:59 +0530

    added search all function

--- src/main/java/com/example/AirlineReservationSystem/repository/FlightRepository.java
+++ src/main/java/com/example/AirlineReservationSystem/repository/FlightRepository.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.AirlineReservationSystem.model.Flight;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, UUID> {
+    List<Flight> findFlightByInformation(Collection<UUID> flightId, String extension);
 }
